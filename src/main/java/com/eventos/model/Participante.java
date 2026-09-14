2fec3e46b3cabd41dc0716b305311a8dd4f43376
GABRIEL TEIXEIRA ALVES DAMACENO <gabriel.damaceno4@senaisp.edu.br>
2026-09-14 10:26:56 -0300

    feat: adicionando model de evento, inscricao e participante

---
+++ src/main/java/com/eventos/model/Participante.java
@@ -0,0 +1,46 @@
+package com.eventos.model;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "participante")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Participante {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "O nome do participante é obrigatório")
+    @Column(nullable = false)
+    private String nome;
+
+    @NotBlank(message = "O e-mail é obrigatório")
+    @Email(message = "O e-mail informado não é válido")
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @OneToMany(mappedBy = "participante", cascade = CascadeType.ALL, orphanRemoval = true)
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    private List<Inscricao> inscricoes = new ArrayList<>();
+}
