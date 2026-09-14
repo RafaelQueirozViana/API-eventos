61f77b96adec2aa2fdbaccf82cc6677bc48f481a
RafaelQueirozViana <rafaelviana.contact@gmail.com>
2026-09-14 11:26:17 -0300

    creating eventoService and participanteService

--- src/main/java/com/eventos/service/EventoService.java
+++ src/main/java/com/eventos/service/EventoService.java
@@ -7,16 +7,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EventoService {
 
     @Autowired
     private EventoRepository eventoRepository;
-
-
     public List<Evento> listarTodos() {
         return eventoRepository.findAll();
     }
+
+    public Optional<Evento> buscarPorId(Long id) {
+        return eventoRepository.findById(id);
+    }
+
+    public Evento salvar(Evento evento) {
+        return eventoRepository.save(evento);
+    }
+
+
+
+
+
 
 }
--- src/main/java/com/eventos/service/ParticipanteService.java
+++ src/main/java/com/eventos/service/ParticipanteService.java
@@ -1,4 +1,30 @@
 package com.eventos.service;
 
+
+import com.eventos.model.Evento;
+import com.eventos.model.Participante;
+import com.eventos.repository.EventoRepository;
+import com.eventos.repository.ParticipanteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class ParticipanteService {
+
+    @Autowired
+    private ParticipanteRepository participanteRepository;
+    public List<Participante> listarTodos() {
+        return participanteRepository.findAll();
+    }
+
+    public Optional<Participante> buscarPorId(Long id) {
+        return participanteRepository.findById(id);
+    }
+
+    public Participante salvar(Participante participante) {
+        return participanteRepository.save(participante);
+    }
 }
