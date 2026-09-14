07c4377b628ac5db8276dc999de1f9465627ef31
RafaelQueirozViana <rafaelviana.contact@gmail.com>
2026-09-14 10:49:53 -0300

    updated repository

--- src/main/java/com/eventos/repository/EventoRepository.java
+++ src/main/java/com/eventos/repository/EventoRepository.java
@@ -1,10 +1,11 @@
 package com.eventos.repository;
 
+
+import com.eventos.model.Evento;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
-public interface EventoRepository {
-
-
-
+public interface EventoRepository  extends JpaRepository<Evento, Long> {
 }
